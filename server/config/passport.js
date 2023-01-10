2e17c2465e562b7243acd356f5d096d7112c928b
PB-Liu <mindware2000@gmail.com>
2023-01-10 10:57:42 -0500

    start google oauth

---
+++ server/config/passport.js
@@ -0,0 +1,72 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+var express = require('express');
+const bcrypt = require("bcryptjs"); 
+
+const db = require('../models/movieModel'); 
+
+
+//helper function: check if user email exists 
+const emailExists = async (email) => {
+    const user = await db.query("SELECT * FROM users WHERE email=$1", [email]);
+
+    if (user.rows.length == 0) return false; 
+      else return user.rows[0];
+  };
+  
+  
+  //helpfunction: verify user 
+  const verifyUser = async (inputPassword, hashedPassword) => {
+    const isMatch = await bcrypt.compare(inputPassword, hashedPassword); 
+    return isMatch; 
+  }
+
+//callback function for local strategy 
+
+const verifyCallback = async (email, password, done) => {
+    try {
+          const user = await emailExists(email);
+          if (!user) return done(null, false);
+          const isMatch = await verifyUser(password, user.password);
+          if (!isMatch) return done(null, false); 
+          return done(null, {id: user.id, email: user.email});
+        } catch (error) {
+          return done(error, false);
+     }
+
+}
+
+//create new local strategy 
+passport.use('local', new LocalStrategy({
+            usernameField: "email",
+            passwordField: "password",
+          }, verifyCallback))
+
+//create google oauth2 stratgey 
+passport.use('google',
+    new GoogleStrategy({
+        // options for google strategy
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        callbackURL: '/user/google/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+        // passport callback function
+        console.log('passport callback function fired:');
+        console.log(profile);
+    })
+);
+// To be finished ....
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+        return cb(null, user.id);
+    });
+    });
+    
+passport.deserializeUser(function(id, cb) {
+db.query('SELECT * FROM users WHERE id = $1', [ id ], function(err, user) {
+    if (err) { return cb(err); }
+    return cb(null, user.rows[0]);
+});
+});
+      
--- server/routes/user.js
+++ server/routes/user.js
@@ -14,4 +14,15 @@ router.get('/logout', (req, res, next)=> {
     req.logout(); 
     res.redirect(); 
 })
+
+// auth with google+
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile']
+}));
+
+// callback route for google to redirect to
+router.get('/google/redirect', (req, res) => {
+    res.send('you reached the redirect URI');
+});
+
 module.exports = router;
