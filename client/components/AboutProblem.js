635610f3fae64980c178129c5169d8c6f59b523a
Jason Brown <superbunker@gmail.com>
2023-01-14 13:04:21 -0800

    presentation draft
    Co-authored-by: nAndrewT <nAndrewT@users.noreply.github.com>

--- client/components/AboutProblem.js
+++ client/components/AboutProblem.js
@@ -20,7 +20,7 @@ function AboutProblem() {
 
     <h1>About the Problem!!</h1>
 
-    <p className="lead">The Problem is Media.</p>
+    <p className="megalead">The Problem is Media.</p>
 
     <p className="lead">How do you keep track of it all??</p>
 
@@ -28,12 +28,13 @@ function AboutProblem() {
     <h2>Solution!!</h2>
 
     <img src="/assets/images/icon-conveyor-claw.png" className="icon" />
-    <p className="lead">GRAB the Media Info</p>
+    <p className="lead">GRAB the Media Info...</p>
 
     <img src="/assets/images/icon-conveyor-load.png" className="icon" />
-    <p className="lead">and PUT it in YOUR OWN LIST!!</p>
+    <p className="lead">...and PUT it in YOUR OWN LIST!!</p>
 
-    <h3 className="next-link">Demo!! then <a href="/about-tech">Tech</a></h3>
+    <img src="/assets/images/icon-unit-stack.png" className="icon" />
+    <h3 className="next-link">Demo!! then <a href="/about-tech">Tech!!</a></h3>
 
     </main>
     </>
--- client/components/AboutTech.js
+++ client/components/AboutTech.js
@@ -20,10 +20,30 @@ function AboutTech() {
 
     <h1>About the Technology!!</h1>
 
-      <p>A page with Public Info about MEDIA CLAW</p>
 
-    <img src="/assets/images/mediaclaw-f55c47.png" className="logo" />
+    <img src="/assets/images/icon-toolbox.png" className="icon" />
+    <h2>Tools Used</h2>
 
+    <ul>
+        <li>React Bootstrap</li>
+        <li>Axios</li>
+        <li>SQL</li>
+        <li>OAuth</li>
+        <li>Passport</li>
+    </ul>
+
+    <br /><br />
+
+    <img src="/assets/images/icon-challenges-small.png" className="icon" />
+    <h2>Technical Challenges</h2>
+    <p className="lead">It's often the LITTLE THINGS!!</p>
+
+    <ul>
+        <li>PostgreSQL gets <code>angry</code> about camelCase—<i>USE UNDERSCORES!!</i></li>
+        <li>Keeping track of props and state in a flurry of components. <i>SO MANY!!</i></li>
+        <li>We were mostly good with GitHub, <i>UNTIL WE WEREN'T!!</i></li>
+        <li>Scope!! We have no sharing or social features, and that is <i>OKAY!!</i></li>
+    </ul>
 
     </main>
     </>
